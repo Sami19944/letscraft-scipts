43f2ccbd4e4e3f70a4483b8a214ff704490addf6
Sami <sami.ansawi@tu-dortmund.de>
2019-06-14 23:58:08 +0200

    work

--- start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-@title minecraft server
-
-echo starting minecraft server
-cd server
-java -Xms1024M -Xmx1024M -jar -DIReallyKnowWhatIAmDoingISwear ../jars/spigot-1.14.2.jar -o true
-
-PAUSE
