ced110b9564d0f045679e8a191b3f912b0419847
Sami <sami.ansawi@tu-dortmund.de>
2019-06-15 01:29:52 +0200

    start_server.py

--- dev.py
+++ dev.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+
+import sys
 import os
 import shutil
 from shutil import copyfile
@@ -11,13 +14,16 @@ jar_folder = "jars/"
 # config
 plugins = ['dev', 'network', 'clouddata']
 
+# letscraft-dev
 def sync_jars():
+	print("sync_jars")
 	if not os.path.exists(plugin_folder):
 	    os.makedirs(plugin_folder)
 	# copy plugins
 	for pl in plugins:
 		src = "../letscraft-api/plugin_" + pl + "/target/" + pl + ".jar"
-		dst = plugin_cache + pl + ".jar"
+		dst = plugin_folder + pl + ".jar"
+		print("   copy " + src + " -> " + dst)
 		copyfile(src, dst)
 		
 def sync_server():
@@ -27,13 +33,14 @@ def sync_server():
 	for pl in plugins:
 		src = "jars/plugins/" + pl + ".jar"
 		dst = path + pl + ".jar"
+		print("   copy " + src + " -> " + dst)
 		copyfile(src, dst)
 
 def clear():
+	print("clearing: " + server_folder)
 	if os.path.exists(server_folder):
 		shutil.rmtree(server_folder) 
 	os.makedirs(server_folder)
-	print("cleared: " + server_folder)
 	
 def copy_template():
 	if not os.path.exists(server_folder):
@@ -51,10 +58,55 @@ def copy_template():
 	for file in template_files:
 		src = template_folder + file
 		dst = server_folder + file
-		print("copying server jar: " + src + " -> " + dst)
+		print("   copy " + src + " -> " + dst)
 		copyfile(src, dst)
 
+def start():
+	if len(sys.argv) < 3:
+		print("missing argument server_folder")
+		sys.exit()
+	execfile(server_folder + "start_server.py")
+
+# console stuff
+def printHelp():
+	print("avaible functions:")
+	print("   sync_jars")
+	print("   sync_server")
+	print("   clear")
+	print("   copy_template")
+	print("   start <server>")
+
 if __name__ == "__main__":
-	clear()
-	copy_template()
-	sync_server()
+	path = sys.argv[0]
+	print("letscraft-dev")
+	if(len(sys.argv) < 2):
+		print("missing argument function")
+		printHelp()
+		sys.exit()
+	
+	func = sys.argv[1]
+	# sync_jars
+	if func == "sync_jars":
+		sync_jars()
+		sys.exit()
+	# sync_server
+	if func == "sync_server":
+		sync_server()
+		sys.exit()
+	# clear
+	if func == "clear":
+		clear()
+		sys.exit()
+	# copy_template
+	if func == "copy_template":
+		copy_template()
+		sys.exit()
+	if func == "start":
+		start()
+		sys.exit()
+	
+	# invalid
+	print("invalid command")
+	printHelp()
+	sys.exit()
+	
--- template/start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-@title minecraft server
-
-:start
-echo starting minecraft server
-java -Xms1024M -Xmx1024M -jar -DIReallyKnowWhatIAmDoingISwear ../jars/spigot-1.14.2.jar -o true
